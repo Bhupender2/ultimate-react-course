4045601b30aefa89d5f113e05c12063e01fbbe1c
Bhupender2 <b.mehtaa2021@gmail.com>
2024-01-13 02:58:52 +0530

    importing css and creating props

--- pizzaa/src/index.js
+++ pizzaa/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 const pizzaData = [
   {
     name: "Focaccia",
@@ -45,48 +46,71 @@ const pizzaData = [
   },
 ];
 
-
 function App() {
-  return <div>
-   <Header/>
-    <Menu/>
-    <Footer />
-  </div>;
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
-function Header(){
-  return <h1>Fast React Pizza Co.</h1>
-}
-
-function Menu(){
-return <div>
-  <h2>Our Menu</h2>
-  <Pizza/>
-  <Pizza/>
-  <Pizza/>
-  <Pizza/>
-</div>
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
-function Footer(){
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        price={23}
+        photoName="pizzas/spinaci.jpg"
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        price={12} // entering JS mode when you want to pass something that is not a string (it can be anthing object , arrays `)
+        photoName="pizzas/funghi.jpg"
+      />
+    </main>
+  );
+}
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p> {props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
   //without jsx we need to use react.createElement
- //return React.createElement('footer', null , 'We are Currently Open')
+  //return React.createElement('footer', null , 'We are Currently Open')
 
- const hour = new Date().getHours();
- const openHour= 10;
- const closeHour=22;
-const isOpen=hour>=openHour && hour<=closeHour
-console.log(isOpen)
- 
- // with jsx
- return <footer>{new Date().toLocaleTimeString()}. We are Currently Open !!</footer>
-}
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
 
-function Pizza(){
-  return<div>
-     <h2>Pizza</h2>
-     <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    <img src="pizzas/spinaci.jpg" alt="food"/>  
-  </div>
+  // with jsx
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We are Currently Open !!
+    </footer>
+  );
 }
 
 // now all we need to do is to render this in the dom so need to create root element fo this
