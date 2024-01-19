6238df9780972a883db7a30f754f8db973336401
Bhupender2 <b.mehtaa2021@gmail.com>
2024-01-19 22:56:28 +0530

    form is created

--- travel-listt/src/App.js
+++ travel-listt/src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 2, packed: true },
 ];
 
 export default function App() {
@@ -19,15 +20,46 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventdefault();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your Trip 😍 ?</h3>
-    </div>
+      <select>
+        {/* basically used for selecting the options*/}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num}>{num}</option>
+        ))}
+      </select>
+      <input type="text" placeholder="items..." />{" "}
+      {/* its the basic input tag where we basically enter the text we wanted*/}
+      <button>add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
